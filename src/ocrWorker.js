84e4c5310fb3395829448ff222ef88b2d9030319
VGImission <VGImission@VGI-MISSION.rabbitdc.com>
2024-10-16 00:12:01 +0700

    add worker1

--- src/ocrWorker.js
+++ src/ocrWorker.js
@@ -1,21 +1,21 @@
 // ocrWorker.js
 import Tesseract from 'tesseract.js';
 
-self.onmessage = async (e) => {
+globalThis.onmessage = async (e) => {
   const { imageURL, workerOptions } = e.data;
 
   try {
     const result = await Tesseract.recognize(imageURL, 'eng+tha', {
       ...workerOptions,
       tessedit_char_whitelist: '0123456789- .',
-      psm: 7, // Process the image as a single line of text
+      psm: 7, // Treat the image as a single line of text
       logger: (message) => {
-        self.postMessage({ type: 'progress', message });
+        globalThis.postMessage({ type: 'progress', message });
       }
     });
 
-    self.postMessage({ type: 'result', result: result.data.text });
+    globalThis.postMessage({ type: 'result', result: result.data.text });
   } catch (error) {
-    self.postMessage({ type: 'error', error: error.message });
+    globalThis.postMessage({ type: 'error', error: error.message });
   }
 };
